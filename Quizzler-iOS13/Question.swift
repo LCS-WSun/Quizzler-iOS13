8099832bde356b78fd903a7d7b40b051847aed26
Wesley Sun <wsun@lcs.on.ca>
2020-04-15 12:36:50 -0400

    created a structure and now using it to hold the questions

---
+++ Quizzler-iOS13/Question.swift
@@ -0,0 +1,16 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Sun, Wesley on 2020-04-15.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+
+struct Question {
+    let text: String
+    let answer: String
+}
--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -16,16 +16,50 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
+    let quiz = [
+        Question(text: "Four + two is equal Six", answer: "True")
+        Question(text: "Five - Three is greater than One", answer: "True")
+        Question(text:)
+        
+        
+        
+    ["Three + Eight is less than ten", "False"]
+    ]
     
+   var questionNumber = 0
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+      updateUI()
+        
     }
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+    let userAnswer = sender.currentTitle
+    let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer {
+        print("Right!")
+        }else {
+        print("Wrong!")
+        }
+        
+        if questionNumber + 1 < quiz.count {
+        questionNumber += 1
+        } else {
+         questionNumber = 0
+        }
+            updateUI()
+        
     }
     
+    func updateUI() {
+      questionLabel.text = quiz[questionNumber][0]
+     
 }
 
+}
